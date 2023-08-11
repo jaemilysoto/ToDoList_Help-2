f160d56c34fb14010d74d8b9787b85419e38f3bf
Jaemily M. Soto <jsoto82@bostonk12.org>
2023-08-11 10:51:52 -0400

    initial commit

---
+++ ToDoList_Help/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoList_Help
+//
+//  Created by scholar on 6/19/23.
+//
+
+//** This file contains all the code for the ToDoItem class**
+
+import Foundation
+
+//Create the ToDoItem class in this file
+
+    //The initializer goes at the very bottom of the class (before the closing curly bracket)
--- ToDoList_Help/ToDoList_HelpApp.swift
+++ ToDoList_Help/ToDoList_HelpApp.swift
@@ -5,13 +5,31 @@
 //  Created by scholar on 6/19/23.
 //
 
+//** This file is where will we access Core Data in Iteration 2**
+
 import SwiftUI
 
+class ToDoItem {
+    
+    var title=""
+    var isImportant = false
+    
+    init(title:String, isImportaant: Bool = false){
+        self.title = title
+        self.isImportant = isImportant
+    }
+    
+    
+}
 @main
 struct ToDoList_HelpApp: App {
+    
+    //Add let persistenceController statement here
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            //Attach the environment modifier inside the parenthesis of ContentView() above
         }
     }
 }
